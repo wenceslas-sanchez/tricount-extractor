0e3e8b9a05a05311f7dc5af46b2cab859aed3b59
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2026-04-30 16:20:07 -0400

    frature: extract custom categories

--- tricount_extractor/models/entry.py
+++ tricount_extractor/models/entry.py
@@ -53,7 +53,7 @@ class Entry:
             payer_uuid=payer["uuid"],
             payer_name=payer["alias"]["display_name"],
             allocations=[Allocation.from_json(a) for a in data["allocations"]],
-            category=data.get("category", "UNCATEGORIZED"),
+            category=cls._extract_category(data),
             urls=cls._extract_attachment_urls(data),
         )
 
@@ -97,3 +97,9 @@ class Entry:
                     continue
                 urls.append(url)
         return urls
+
+    @staticmethod
+    def _extract_category(data: dict) -> str:
+        if (custom_category := data.get("category_custom")) is not None:
+            return custom_category
+        return data.get("category", "UNCATEGORIZED")
